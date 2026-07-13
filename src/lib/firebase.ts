9eab26d708a29f5ad48fa4c81f6e7cc3185479b4
hugoaalen <hugoaalen@gmail.com>
2026-07-13 16:30:13 +0200

    fix: proxy firebase auth on vercel

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,10 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
-  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain:
+    typeof window !== "undefined" && window.location.hostname === "hugoalen.vercel.app"
+      ? "hugoalen.vercel.app"
+      : import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
