3250590e341c2dda3e06d28525d70c01f1b49f42
hugoaalen <hugoaalen@gmail.com>
2026-07-13 16:59:04 +0200

    Revert "feat: add project image uploads"
    This reverts commit 6ec903a6cf2997d30f1a9f368781d7b62910a26d.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
-import { getStorage, type FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
@@ -33,6 +32,5 @@ export const firebaseApp: FirebaseApp = getApps().length
 
 export const auth: Auth = getAuth(firebaseApp);
 export const db: Firestore = getFirestore(firebaseApp);
-export const storage: FirebaseStorage = getStorage(firebaseApp);
 export const googleProvider = new GoogleAuthProvider();
 export const adminEmail = import.meta.env.PUBLIC_ADMIN_EMAIL;
